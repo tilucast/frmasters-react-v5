14c6e4c5e0a1d6b09191ef62ecb160af7bfc61c1
Lucas <tilucast@gmail.com>
2021-04-30 21:17:46 -0300

    commited before the carousel section

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import pet from "@frontendmasters/pet";
 
 const Details = (props) => {
+  const [animal, setAnimal] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    pet.animal(props.id).then(({ animal }) => {
+      setAnimal({
+        animal: animal.name,
+        name: animal.name,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+      });
+      setLoading(false);
+    }, console.error);
+  }, [props.id]);
+
+  if (loading) return <h1>Loading ...</h1>;
+
+  const { name, breed, location, description, animal: animalApi } = animal;
+
   return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
+    <div className="details">
+      <div className="">
+        <h1>{name}</h1>
+        <h2>{`${animalApi} - ${breed} - ${location}`}</h2>
+        <button>Adopt {name}!</button>
+        <p>{description}</p>
+      </div>
+    </div>
   );
 };
 
